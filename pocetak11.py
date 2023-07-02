916df1ab07ce760262031adf27c55e52a0cd20d5
Arijan <arijannp2005@gmail.com>
2023-07-02 18:42:51 +0200

    new commit

---
+++ pocetak11.py
@@ -0,0 +1,21 @@
+# import re
+# txt = "There is rain in Spain"
+# x = re.findall("ai",txt)
+# print(x)
+
+# try:
+#     print(x)
+# except NameError:
+#     print("nesto")
+# except:
+#     print("nesto drugo")
+
+# x = 5
+# try:
+#   print(x)
+# except:
+#   print("Something went wrong")
+# else:
+#   print("bravo")
+# finally:
+#   print("The 'try except' is finished")
