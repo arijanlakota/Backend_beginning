f7d7f03a84dd99d4332f3fabbcdc3a4d9abde254
Arijan <arijannp2005@gmail.com>
2023-08-01 17:48:39 +0200

    json

---
+++ pocetak23.py
@@ -0,0 +1,23 @@
+#json
+#1
+import json
+# pyDict = json.loads('{ "Name":"David", "Class":"I", "Age":6 }')
+# print(pyDict)
+#2
+# pythonObject = {"elvedin":1,"arijan":2}
+# jsonObject = json.dumps(pythonObject)
+#3
+# pythonObject = {"hasan":1,"suvo":1}
+# jsonObject = json.dumps(pythonObject)
+# print(type(jsonObject))
+#4
+pythonObject = {
+    'wow':
+    {"yeah":
+     {"cool":
+      {"bro":{
+          "shu":2
+      }}
+      }
+    }
+}
